cfd7fc08ea1572712da5da6dc49bd768b136f31b
lividpower <matheusmontenegro100@gmail.com>
2025-09-12 20:50:32 -0300

    Observação interessante a ser feita!

--- programacao_orientada_a_objetos/aulas/aula_3/TurmaAlunos.java
+++ programacao_orientada_a_objetos/aulas/aula_3/TurmaAlunos.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class TurmaAlunos {
+    public static double CalculaMedia(Aluno []turma) {
+        
+    }
+
     public static void main(String []args) {
 
         //declarações iniciais
@@ -13,11 +17,13 @@ public class TurmaAlunos {
         System.out.println("Registrando o nome e média dos alunos da turma...\n");
 
         for(int i = 0; i < turma.length; i++) {
-            Scanner scanner = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in); //estou criando um objeto diferente dentro de cada iteração do for()
             System.out.println("Digite o nome do respectivo aluno: ");
-            turma[i].nome = scanner.nextLine();
+            turma[i].nome = scanner.nextLine(); 
             System.out.print("Digite a média do respectivo aluno: \n");
-            turma[i].media = scanner.nextDouble();
+            turma[i].media = scanner.nextDouble(); //quando eu uso desse método, ele lê um double, porém, o buffer de leitura armazena também o \n referente a tecla Enter
+            //esse \n não é lido pela função nextDouble() e daí a próxima chamada a nextLine() irá acabar lendo esse caractere!
+            //quando eu crio um objeto "scanner" a cada iteração, eu estou evitando que isso aconteça, porém, estou criando um overhead apenas conseguir fugir desse bug
             acumulador += turma[i].media;
             System.out.println("");
         }
