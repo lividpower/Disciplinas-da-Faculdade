f6fb9902079ad8c9ae27b6a0fed498dc6f705b2e
lividpower <matheusmontenegro100@gmail.com>
2025-09-17 01:54:51 -0300

    Necessário entender o por quê do código dar errado quando implemento a busca binária dessa forma!

--- organizacao_de_estruturas_de_arquivos/aulas/aula_4/BuscaBinariaCEP.c
+++ organizacao_de_estruturas_de_arquivos/aulas/aula_4/BuscaBinariaCEP.c
@@ -35,19 +35,17 @@ int main(int argc, char**argv)
 	//inicializando as variáveis da busca binária
 	fseek(f, 0, SEEK_SET);
 	inicio = ftell(f);
-	inicio = inicio / sizeof(Endereco);
 	if(inicio == -1) {
 		fprintf(stderr, "Erro na função ftell()\n");
 	}
 	fseek(f, 0, SEEK_END);
 	fim = ftell(f) - 1; //como iremos trabalhar com indexação, precisamos subtrair 1 para conseguir manipular posteriormente os indíces das strings
-	fim = fim / sizeof(Endereco); //estamos agora trabalhando com a indexação das diferentes estruturas Endereco que existem dentro do arquivo cep_ordenado.dat
 	if(fim == -1) {
 		fprintf(stderr, "Erro na função ftell()\n");
 
 	}
 	meio = (inicio + fim) / 2; //dúvida em relação ao posicionamento dentro do array
-	fseek(f, meio * sizeof(Endereco), SEEK_SET);
+	fseek(f, meio, SEEK_SET);
 	qt = fread(&e,sizeof(Endereco),1,f); //iremos ler a estrutura Endereco posicionada no meio do arquivo
 	
 	//chegará uma hora que, após tantas divisões terem sido realizadas, o meio acabará sendo igual o inicio e o fim, indicando que o arquivo está prestes a chegar ao fim
@@ -67,7 +65,7 @@ int main(int argc, char**argv)
 			inicio = meio + 1;
 			meio = (inicio + fim) / 2;
 		}
-		fseek(f, meio * sizeof(Endereco), SEEK_SET); 
+		fseek(f, meio, SEEK_SET); 
 		qt = fread(&e,sizeof(Endereco),1,f);		
 	}
 	printf("Total Lido: %d\n", c); //quantas vezes a função fread() foi executada...
