acc9e8272008ebb4a91f763494b0d2c8ec19d077
lividpower <matheusmontenegro100@gmail.com>
2025-09-26 19:48:55 -0300

    Analisando um possível erro

--- programacao_orientada_a_objetos/aulas/aula_4/Zoo.java
+++ programacao_orientada_a_objetos/aulas/aula_4/Zoo.java
@@ -1,8 +1,8 @@
 public class Zoo {
     public static void main(String []args) {
-        Leao x = new Leao();
+        Animal x = new Leao(); //essa linha de código compila porque a classe Leão está herdando a classe Animal!!!
         x.setNome("Simba");
         System.out.println(x.getVivo()); //variáveis do tipo "boolean" irão ser inicializadas como "false"
-        x.rugir();
+        x.rugir(); //já esta linha não compila, porque Animal.rugir() não estaria correto, posto que o método rugir() é exclusivo da classe Leao
     }
 }
