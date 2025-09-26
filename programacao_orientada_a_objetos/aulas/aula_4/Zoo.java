440a2cc8fa10aa6bf647775d0c919f114baf97fb
lividpower <matheusmontenegro100@gmail.com>
2025-09-26 20:24:22 -0300

    Passagem de parâmetro por referência

--- programacao_orientada_a_objetos/aulas/aula_4/Zoo.java
+++ programacao_orientada_a_objetos/aulas/aula_4/Zoo.java
@@ -1,6 +1,6 @@
 public class Zoo {
     public static void main(String []args) {
-        Animal x = new Zebra();
+        Leao x = new Animal(); //esse código não compila, porque Animal não pode ser atribuído a um tipo Leao, no entanto, o contrário é válido!
         x.setNome("Simba");
         System.out.println(x.getVivo()); //variáveis do tipo "boolean" irão ser inicializadas como "false"
         teste(x); //esse código funciona!
