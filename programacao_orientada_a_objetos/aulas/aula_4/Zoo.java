cd68bb2872f94c7ec6ae7c6d9707a0866195b030
lividpower <matheusmontenegro100@gmail.com>
2025-09-26 20:00:54 -0300

    Passagem de parâmetro por referência

--- programacao_orientada_a_objetos/aulas/aula_4/Zoo.java
+++ programacao_orientada_a_objetos/aulas/aula_4/Zoo.java
@@ -1,8 +1,11 @@
 public class Zoo {
     public static void main(String []args) {
-        Animal x = new Leao(); //essa linha de código compila porque a classe Leão está herdando a classe Animal!!!
+        Animal x = new Zebra();
         x.setNome("Simba");
         System.out.println(x.getVivo()); //variáveis do tipo "boolean" irão ser inicializadas como "false"
-        x.rugir(); //já esta linha não compila, porque Animal.rugir() não estaria correto, posto que o método rugir() é exclusivo da classe Leao
+        teste(x); //esse código funciona!
+    }
+    public static void teste(Animal A) {
+        System.out.println(A.getNome());
     }
 }
