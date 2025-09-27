13020212ca7f3f7a1505e91d59d36d9b0f6f53af
lividpower <matheusmontenegro100@gmail.com>
2025-09-26 21:27:44 -0300

    Sobrescrita do método .toString()

--- programacao_orientada_a_objetos/aulas/aula_4/Zoo.java
+++ programacao_orientada_a_objetos/aulas/aula_4/Zoo.java
@@ -1,15 +1,10 @@
 public class Zoo {
     public static void main(String []args) {
-        Animal w [] =  new Leao[3]; // também é possível atribuir Leao[3] a essa variável!!!
-        w[0] = new Leao();
-        w[1] = new Urso();
-        w[2] = new Elefante();
-        Object o = new Object(); //funciona!!!
-        System.out.println(o);
-        for(int i = 0; i < w.length; i++) {
-            w[i].correr();
-        }
-
+        Leao x = new Leao();
+        x.setNome("Simba");
+        System.out.println(x);
+        Urso y = new Urso();
+        System.out.println(y);
     }
     public static void teste(Animal A) {
         //...        
