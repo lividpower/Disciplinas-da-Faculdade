d7c2dcd6e3ac1514cc9477a8d27cafcd33168c07
lividpower <matheusmontenegro100@gmail.com>
2025-10-03 20:28:33 -0300

    Manipulando o conteúdo de uma string

--- programacao_orientada_a_objetos/aulas/aula_5/Teste.java
+++ programacao_orientada_a_objetos/aulas/aula_5/Teste.java
@@ -1,9 +1,14 @@
 public class Teste {
     public static void main(String args[]) {
         String x[] = {"Ana Maria#18", "Julia#21"};
-        //int u[] = {3, 2, 3};
-        System.out.println(x.length);
-        String m = "Jonas#30";
-        String []k = m.split("#");
+        double soma = 0;
+        for(int i = 0; i < x.length; i++) {
+            String j[] = x[i].split("#");
+            //int i; //se eu crio duas variáveis locais ao for() com nomes iguais, isso me gerará um erro de compilação!
+            int aux = Integer.parseInt(j[1]); //o método parseInt() é estático!
+            soma += aux;
+        }
+        double mediaIdades = soma / x.length;
+        System.out.println("Média das idades: " + mediaIdades);
     }
 }
