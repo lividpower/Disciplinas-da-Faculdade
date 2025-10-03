37166b4ba8252b22f569f4ad2ab000530e6c1be8
lividpower <matheusmontenegro100@gmail.com>
2025-10-03 19:14:06 -0300

    Formas de contornar a sobrescrita de métodos

--- programacao_orientada_a_objetos/aulas/aula_5/Casa.java
+++ programacao_orientada_a_objetos/aulas/aula_5/Casa.java
@@ -3,8 +3,8 @@ public class Casa extends Imovel {
         super(preco); //chamando o construtor da classe acima de forma explícita
     }
 
-    public Casa() {
-        //nesse caso, por já estarmos chamando um construtor, não existirá uma chamada a super() 
-        this(0); //estou chamando o construtor da minha própria classe
+    public double getPrecoFinal() { 
+        //return getPrecoFinal() + (getPrecoFinal() * 0.1); //nesse caso, o método que é chamado é o referente a classe Casa, sobrescrevendo o método com mesma assinatura da classe Imovel
+        return super.getPrecoFinal() + (super.getPrecoFinal() * 0.1); //uma forma de chamar o método da classe acima sem sobrescrever o método
     }   
 }
