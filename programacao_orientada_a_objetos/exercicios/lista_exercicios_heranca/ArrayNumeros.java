e7885ea99e610c667d47a93858bc58d8d6c3a027
lividpower <matheusmontenegro100@gmail.com>
2025-10-07 17:47:50 -0300

    Invertendo a ordem dos elementos no array

--- programacao_orientada_a_objetos/exercicios/lista_exercicios_heranca/ArrayNumeros.java
+++ programacao_orientada_a_objetos/exercicios/lista_exercicios_heranca/ArrayNumeros.java
@@ -2,10 +2,16 @@
 import java.util.Random;
 //java.lang.Math é importado implicitamente pelo compilador 
 import static java.lang.Math.*; //importando os métodos static pertencentes à classe Math 
+import java.util.Arrays;
 
 public class ArrayNumeros {
-    public static int inverteArrayNumeros(int array[]) {
-        return 0;
+    public static int[] inverteArrayNumeros(int array[]) {
+        for(int i = 0; i < (array.length / 2); i++) { //o número de modificações reduz pela metade, independente se o array.length é par ou ímpar!
+            int aux = array[(array.length - 1) - i]; 
+            array[(array.length - 1) - i] = array[i];
+            array[i] = aux;
+            return array; 
+        }
     }
 
     public static void main(String args[]) {
@@ -13,9 +19,10 @@ public class ArrayNumeros {
         //Scanner scanner = new Scanner(System.in); //criando uma instância da classe Scanner
         //Random random = new Random(((long) Math.random())); //passando uma semente uma semente aleatória para o método que irá gerar um número aleatório
         Random random = new Random((long) random()); //Math.random() retorna um double e o construtor Random() apenas pode receber um long, por padrão
-        System.out.println("Preencha o array com números inteiros: ");
         for(int i = 0; i < array.length; i++) {
-            System.out.printf("array[%d] = %d%n", i, random.nextInt(1000)); // %n, nesse caso, representa uma nova linha!
+            array[i] = random.nextInt(1000);
         }
+        System.out.println("Array de Inteiros: " + Arrays.toString(array)); //essa é uma forma bem interessante de imprimirmos um array!!!
+        System.out.println("Array de Inteiros invertido: " + Arrays.toString(inverteArrayNumeros(array)));
     }
 }
