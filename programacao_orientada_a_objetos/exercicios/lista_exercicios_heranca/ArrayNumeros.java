0838690627c90450c13da1de41f1dbb6c52df275
lividpower <matheusmontenegro100@gmail.com>
2025-10-07 18:02:37 -0300

    Estudando como funciona a questão de passagem de parâmetro por referência, também chamado popularmente de ponteiro

--- programacao_orientada_a_objetos/exercicios/lista_exercicios_heranca/ArrayNumeros.java
+++ programacao_orientada_a_objetos/exercicios/lista_exercicios_heranca/ArrayNumeros.java
@@ -6,13 +6,17 @@ import java.util.Arrays;
 
 public class ArrayNumeros {
     public static int[] inverteArrayNumeros(int array[]) { 
-        int []array_ = array; //estou atribuindo o endereço do meu array inicial para essa nova variável temporária array_
+        //int []array_ = array; //estou atribuindo o endereço do meu array inicial para essa nova variável temporária array_ //quando faço isso, o endereço que será alterado é basicamente o mesmo, logo, acaba não surtindo muita diferença
+        int []array_ = new int[array.length]; 
+        for(int i = 0; i < array.length; i ++) {
+            array_[i] = array[i]; //agora sim eu estou criando a cópia do meu array!!!!!!!!!!!!!!!!!
+        }
         for(int i = 0; i < (array.length / 2); i++) { //o número de modificações reduz pela metade, independente se o array.length é par ou ímpar!
             int aux = array_[(array.length - 1) - i]; 
             array_[(array.length - 1) - i] = array_[i];
             array_[i] = aux;
         }
-        return array_; //agora sim eu fiz uma cópia do array original e não alterei ele diretamente, logo, quando eu acessar a main, terei acesso ao meu array original que se manteve intacto
+        return array_; //agora sim eu fiz uma cópia do array original e não alterei ele diretamente, logo, quando eu acessar a main, terei acesso ao meu array original que se manteve intacto 
     }
 
     public static void main(String args[]) {
@@ -25,6 +29,6 @@ public class ArrayNumeros {
         }
         System.out.println("Array de Inteiros: " + Arrays.toString(array)); //essa é uma forma bem interessante de imprimirmos um array!!!
         System.out.println("Array de Inteiros invertido: " + Arrays.toString(inverteArrayNumeros(array)));
-        System.out.println("Array de Inteiros: " + Arrays.toString(array)); //EU ME ENGANEI AO ACHAR QUE EU NÃO ESTARIA ALTERANDO O ARRAY!!!! RSRSRSRSRSRSRSRS
+        System.out.println("Array de Inteiros: " + Arrays.toString(array)); //Isto prova que o array original não foi alterado
     }
 }
