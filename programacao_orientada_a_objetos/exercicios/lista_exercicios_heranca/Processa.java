2c1a8371965603cf6d510f13d7ed344da0f8dae8
lividpower <matheusmontenegro100@gmail.com>
2025-10-09 10:24:50 -0300

    É possível utilizar do método main() de forma recursiva como forma de explorar uma falha de StackOverFlow

--- programacao_orientada_a_objetos/exercicios/lista_exercicios_heranca/Processa.java
+++ programacao_orientada_a_objetos/exercicios/lista_exercicios_heranca/Processa.java
@@ -3,10 +3,8 @@ public class Processa {
 	public static void main(String[] args) {
 		Processa p = new Processa(); 
 		//é possível declarar uma variável referente a classe que estamos construindo, como se fosse algo "recursivo"...estamos chamando a classe dentro da própria classe
-		System.out.println(p.calcula(2, 3)); 
-		//estamos tentando utilizar de um método não-estático, 
-		//mas sem declarar uma instância para esta respectiva classe
-		//poderíamos simplesmente declarar calcula como um método static
+		System.out.println("Chamando o método main() de forma recursiva"); 
+		p.main(args);
 	}
 	public double calcula(double a, double b) {
 		return (a+b) * (a*0.1) + (b*0.9);
