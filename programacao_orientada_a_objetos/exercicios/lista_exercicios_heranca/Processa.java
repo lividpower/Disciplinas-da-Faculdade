0792f687c093204886e5f19311e63caa4314c876
lividpower <matheusmontenegro100@gmail.com>
2025-10-09 10:09:09 -0300

    Podemos definir um tipo da classe dentro de sua própria definição

--- programacao_orientada_a_objetos/exercicios/lista_exercicios_heranca/Processa.java
+++ programacao_orientada_a_objetos/exercicios/lista_exercicios_heranca/Processa.java
@@ -1,11 +1,12 @@
 public class Processa {
-//este código não compila!!!
+
 	public static void main(String[] args) {
-		System.out.println(this.calcula(2,3)); 
+		Processa p = new Processa(); 
+		//é possível declarar uma variável referente a classe que estamos construindo, como se fosse algo "recursivo"...estamos chamando a classe dentro da própria classe
+		System.out.println(p.calcula(2, 3)); 
 		//estamos tentando utilizar de um método não-estático, 
 		//mas sem declarar uma instância para esta respectiva classe
-		//o certo seria utilizar, nesse caso, de this.calcula(2,3)
-		//ou simplesmente declarar calcula como um método static
+		//poderíamos simplesmente declarar calcula como um método static
 	}
 	public double calcula(double a, double b) {
 		return (a+b) * (a*0.1) + (b*0.9);
