dea064bb9608f9f9a09d0f9c127c32454ffe421d
lividpower <matheusmontenegro100@gmail.com>
2025-10-09 09:57:29 -0300

    Estudando erros de compilação possíveis

---
+++ programacao_orientada_a_objetos/exercicios/lista_exercicios_heranca/Processa.java
@@ -0,0 +1,13 @@
+public class Processa {
+//este código não compila!!!
+	public static void main(String[] args) {
+		System.out.println(calcula(2,3)); 
+		//estamos tentando utilizar de um método não-estático, 
+		//mas sem declarar uma instância para esta respectiva classe
+		//o certo seria utilizar, nesse caso, de this.calcula(2,3)
+		//ou simplesmente declarar calcula como um método static
+	}
+	public double calcula(double a, double b) {
+		return (a+b) * (a*0.1) + (b*0.9);
+	}
+}
