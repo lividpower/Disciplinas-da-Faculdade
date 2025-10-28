ccc5c8bcecb830aa1b0674b3818be7a5324d6efc
lividpower <matheusmontenegro100@gmail.com>
2025-10-28 09:27:27 -0300

    Construindo um arquivo de índice

---
+++ organizacao_de_estruturas_de_arquivos/arquivos_cabecalho/endereco.h
@@ -0,0 +1,26 @@
+#ifndef ENDERECO_H //isto daqui são macros de pré-processamento
+#define ENDERECO_H //utilizamos deste if para não inserirmos o mesmo arquivo de cabeçalho dentro de um arquivo
+
+//este é um arquivo de cabeçalhos
+//não é interessante definirmos as funções dentro deste arquivo!
+
+typedef struct _Endereco
+{	//todos esses caracteres juntos formam uma linha com 300 caracteres, justamente igual ao padrão presente dentro do arquivo cep.dat
+	//mas e os caracteres nulos? neste arquivo, não temos caracteres nulos! devemos tomar cuidado com isso ao manipular esses vetores de caractere
+	char logradouro[72]; 
+	char bairro[72];
+	char cidade[72];
+	char uf[72];
+	char sigla[2];
+	char cep[8];
+	char lixo[2]; // caractere de espaço no final da linha + caractere de quebra de linha (20 0A)
+} Endereco;
+
+typedef struct _Indice {
+	long posicao_bytes; //posicionamento em relação aos bytes
+	char cep[8]; 
+} Indice;
+
+int compara(const void *i1, const void *i2);
+
+#endif
---
+++ organizacao_de_estruturas_de_arquivos/aulas/16102025/cria_index.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <endereco.h> //devemos, neste caso, incluir onde que devemos encontrar este arquivo de cabeçalho 
+//utiliza-se de -I caminho_diretorio/ para conseguir indicar onde devemos procurar por este arquivo de cabeçalho
+//caminho_diretorio = /home/mathe/Faculdade/Disciplinas_da_Faculdade/organizacao_de_estruturas_de_arquivos/arquivos_cabecalho
+//caminho_diretorio = ../../arquivos_cabecalho
+//Para compilar -> gcc -I/home/mathe/Faculdade/Disciplinas_da_Faculdade/organizacao_de_estruturas_de_arquivos/arquivos_cabecalho -g -o cria_indice cria_index.c 
+
+//devemos criar um índice
+//devemos fazer a leitura dos CEPs e de suas respectivas posições no arquivo de índice
+//devemos ordenar o arquivo de índice
+//devemos conseguir ler um registro específico a partir de um determinado CEP
+
+int main(int argc, char* argv[]) {
+
+    if(argc == 2) {
+        //declarações
+        FILE *file_indices, *file_cep;
+        long tamanho_arquivo, num_registros; //ftell retorna um long
+        int teste_retorno; //fseek retorna um int
+
+        file_indices = fopen("indices_cep.dat", "wb"); //criando um novo arquivo
+        if(file_indices == NULL) {
+            fprintf(stderr, "Erro ao abrir o arquivo");
+            return 1;
+        }
+        file_cep = fopen("cep.dat", "rb");
+        if(file_cep == NULL) {
+            fprintf(stderr, "Erro ao abrir o arquivo");
+            return 1;
+        }
+        teste_retorno = fseek(file_cep, 0, SEEK_END); //colocando o cursor de leitura ao final do arquivo
+        tamanho_arquivo = ftell(file_cep); 
+        num_registros = tamanho_arquivo / 300;
+        Indice indices[num_registros]; //isto gera um segmentation fault!
+
+        //leitura do arquivo
+        for(int i = 0; i < num_registros; i++) {
+            Endereco buffer_registro;
+            fseek(file_cep, (i * sizeof(Endereco)), SEEK_SET); //percorrendo os registros do arquivo
+            indices[i].posicao_bytes = ftell(file_cep); //ftell retorna um tipo long
+            fread(&buffer_registro, sizeof(Endereco), 1, file_cep);
+            strncpy(indices[i].cep, buffer_registro.cep, 8); //esta função altera o valor da string
+        }
+
+        //ordenação e escrita dos índices
+        qsort(indices, num_registros, sizeof(Indice), compara);
+        fwrite(indices, sizeof(Indice), num_registros, file_indices);
+    }
+    else {
+        fprintf(stderr, "USO: %s [CEP]\n", argv[0]);
+        return 1;
+    }
+}
+
+// compara: função que compara dois endereços
+int compara(const void *i1, const void *i2) {
+    return strncmp(((Indice*)i1)->cep, ((Indice*)i2)->cep, 8);
+}
