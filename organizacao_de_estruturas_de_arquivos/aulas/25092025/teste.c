c437579cb08e008395815452a23513b517ea68b5
lividpower <matheusmontenegro100@gmail.com>
2025-11-01 09:19:51 -0300

    Resolvendo o problema de segfault

--- organizacao_de_estruturas_de_arquivos/aulas/25092025/teste.c
+++ organizacao_de_estruturas_de_arquivos/aulas/25092025/teste.c
@@ -7,8 +7,10 @@ int main() {
     int retorno = snprintf(string, 0, "Hello"); //retorna o tamanho da string "Hello" sem contar com o \0
     printf("%d\n", retorno);
 
-    char *string_ = "Oi";
-    int retorno_ = snprintf(string_, strlen(string_) + 1, "Hello"); //este código gerou um segmentation fault!
+    char *string_ = "Oi"; //estou definindo uma string literal em região de memória com permissão apenas de leitura!
+    int retorno_ = snprintf(string_, strlen(string_) + 1, "Hello"); 
+    //estou tentando escrever em uma string que foi alocada em região de memória de apenas leitura!!!!
+    //este código gerou um segmentation fault por eu tentar escrever em região de memória não autorizada para escrita!!!!
     printf("%d\n", retorno_);
 
     return 0;
